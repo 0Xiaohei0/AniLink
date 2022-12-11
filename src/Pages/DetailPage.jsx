953112a9642343c5156f5046a65dd1ca0758acc3
0Xiaohei0 <15852786007ysc@gmail.com>
2022-12-10 23:35:24 -0700

    Detail Page layout

--- src/Pages/DetailPage.jsx
+++ src/Pages/DetailPage.jsx
@@ -1,3 +1,4 @@
+import "./DetailPage.css";
 import React from "react";
 import { useLocation } from "react-router-dom";
 
@@ -6,19 +7,29 @@ function DetailPage() {
   var { imageUrl, name, type, description, episodes } = location.state;
   const watchedArray = new Array(episodes).fill(false);
   return (
-    <div>
-      <img alt="coverImage" className="Details--image" src={imageUrl} />
-      <h1>{name}</h1>
-      <p>{description}</p>
-      <p>Type: {type}</p>
-      {watchedArray.map((watchedBool, index) => (
-        <button key={index}>
-          <p>{index}</p>
-          <p>{watchedBool}</p>
-        </button>
-      ))}
+    <div className="PageContainer">
+      <div className="Banner">
+        <img alt="coverImage" className="image" src={imageUrl} />
+        <div>
+          <h1>{name}</h1>
+          <p>{description}</p>
+          <p>Type: {type}</p>
+        </div>
+      </div>
+      <div className="EpisodeButtonContainer">
+        {watchedArray.map((watchedBool, index) => (
+          <button key={index} className="EpisodeButton">
+            <p>{index}</p>
+            <p>{watchedBool}</p>
+          </button>
+        ))}
+      </div>
+      <input className="EpisodeInput" type="number" />
       <p>URL: Not SET</p>
-      <input type="text" />
+      <div className="URLSetter">
+        <input className="URLInput" type="text" />
+        <button className="URLSet EpisodeButton">Set URL</button>
+      </div>
     </div>
   );
 }
