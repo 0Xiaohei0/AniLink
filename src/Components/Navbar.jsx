8b1089472575141760ca51f5d17f4cf1a0781323
0Xiaohei0 <15852786007ysc@gmail.com>
2022-12-11 21:32:25 -0700

    unsaved changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,3 @@
 import React from "react";
 
-export default function NavBar({ setSearch }) {
-  const handleChange = (event) => {
-    setSearch(event.target.value);
-  };
-  return (
-    <div className="Navbar">
-      <input className="SearchBar" type="text" onChange={handleChange} />
-    </div>
-  );
-}
+export default function NavBar({ setSearch }) {}
