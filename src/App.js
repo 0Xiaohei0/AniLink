dbbe42f9660fd8184da5e4e8939972254bc287ae
0Xiaohei0 <15852786007ysc@gmail.com>
2022-12-12 18:33:22 -0700

    Password login

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import SearchPage from "./Pages/SearchPage";
 import DetailPage from "./Pages/DetailPage";
 import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SearchPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/detail/:id" element={<DetailPage />} />
     </Routes>
   );
--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -4,19 +4,23 @@ import React from "react";
 function LoginPage() {
   return (
     <div className="LoginPage--Container">
-      <h1>Login</h1>
-      <form action="/login" method="POST" className="LoginPage--Form">
+      <h1>Register</h1>
+      <form action="/register" method="POST" className="LoginPage--Form">
         <div className="LoginPage--InputPair">
           <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" required />
         </div>
         <div className="LoginPage--InputPair">
+          <label htmlFor="userName">Username</label>
+          <input type="userName" id="userName" name="userName" required />
+        </div>
+        <div className="LoginPage--InputPair">
           <label htmlFor="password">Password</label>
           <input type="password" id="password" name="password" required />
         </div>
-        <button type="submit">Login</button>
+        <button type="submit">Register</button>
       </form>
-      <a href="/register">Register</a>
+      <a href="/login">Login</a>
     </div>
   );
 }
